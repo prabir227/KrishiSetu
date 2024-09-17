61f1a97e8f035d1c8719116be4160a769639ebbe
Prabir Ghosh <prabirghosh227@gmail.com>
2024-09-17 22:34:05 +0530

    added one ui element to marketplace

--- lib/marketplace.dart
+++ lib/marketplace.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating/flutter_rating.dart';
 import 'package:krishi_setu/homepage.dart';
 
 class MarketplacePage extends StatefulWidget {
@@ -9,6 +10,7 @@ class MarketplacePage extends StatefulWidget {
 
 class MarketplacePageState extends State<MarketplacePage> {
   int currentIndex = 0;
+  var searchController = TextEditingController();
 
 
   @override
@@ -61,13 +63,28 @@ class MarketplacePageState extends State<MarketplacePage> {
             padding: const EdgeInsets.all(15.0),
             child: Container(
               height: 50,
-              child: Container(width: 60,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 300,
+                    child: TextField(
+                      controller: searchController,
+                      decoration: InputDecoration(
 
-                child: Icon(Icons.search_rounded,
-                color: Colors.white,),
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                ),
+                      ),
+                    ),
+                  ),
+                  Container(width: 60,
+                    height: 50,
+
+                    child: IconButton(onPressed: (){}, icon: Icon(Icons.search_rounded,color: Colors.white,)),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(5),
+
+                    ),
+                  ),
+                ],
               ),
               decoration: BoxDecoration(
                 border: Border.all(
@@ -76,6 +93,57 @@ class MarketplacePageState extends State<MarketplacePage> {
                 borderRadius: BorderRadius.circular(5),
 
               ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: (){},
+                  child: Container(
+                    decoration:BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Colors.grey
+                      )
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Container(
+
+                              child: Image.asset("asset/images/tomato.png",),
+                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              child: Column(
+                                children: [
+                                  Text("Tomatoes"),
+                                  StarRating(rating: 4,),
+                                  Text("Good quality tomatoes are present here."),
+                                  Text("All are grown using good seeds"),
+                                  Text("State: Karnataka, District, Bengaluru"),
+                                  Row(
+                                    children: [
+                                      Text("Price:Rs 30/Kg"),
+                                      Text("Land: 5 Acres")
+                                    ],
+                                  )
+
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+              ],
             ),
           )
         ],
